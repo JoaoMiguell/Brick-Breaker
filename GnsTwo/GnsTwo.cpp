78357edfb908c9b3fe9ec7251f33c84c0991ccc4
Joao Miguell <joaomiguel_sm@outlook.com>
2023-07-15 14:40:15 -0300

    end-game done

---
+++ GnsTwo/GnsTwo.cpp
@@ -0,0 +1,106 @@
+﻿#include "GnsTwo.h"
+
+int main(void) {
+	const int screenW{ 610 }, screenH{ 550 };
+	Rectangle player{ (float)screenW / 2 - 30, (float)screenH - 50, 60.0f,20.f };
+	int lastDirectionPlayer{ 1 };
+	std::vector<Rectangle> bricks(50);
+	Ball ball{ {screenW / 2, screenH / 2} };
+	Rectangle deathArea{ 0, screenH - 45, screenW, 40 };
+	std::string textLifes{std::format("Vidas: {}", ball.lifes)};
+	bool exitWindow{ false };
+	int noBricks{ 0 };
+	int count{ 0 };
+
+	genScenario(bricks);
+	InitWindow(screenW, screenH, "My game");
+	SetTargetFPS(60);
+
+	while(!exitWindow) {
+		// Moviments
+		if(IsKeyDown(KEY_LEFT)) {
+			player.x -= 5;
+			lastDirectionPlayer = -1;
+		}
+		if(IsKeyDown(KEY_RIGHT)) {
+			player.x += 5;
+			lastDirectionPlayer = 1;
+		}
+
+		ball.move(screenW, screenH);
+
+		// Objects Collisions
+		if(CheckCollisionCircleRec(ball.position, ball.size, player))
+			ball.collide('p', lastDirectionPlayer);
+
+		for(auto i{ bricks.begin() }; i != bricks.end(); ++i) {
+			if(CheckCollisionCircleRec(ball.position, ball.size, { i->x, i->y, i->width, i->height })) {
+				i->x = 0;
+				i->y = 0;
+				i->width = 0;
+				i->height = 0;
+				ball.collide('b');
+				noBricks++;
+			}
+		}
+
+		if(CheckCollisionCircleRec(ball.position, ball.size, deathArea)) {
+			ball.hit(screenW, screenH);
+			textLifes = std::format("Vidas: {}", ball.lifes);
+		}
+
+
+		// Border Collisions
+		if((ball.position.x >= (screenW - ball.size)) || (ball.position.x <= ball.size))
+			ball.collide('w');
+		if((ball.position.y >= (screenH - ball.size)) || (ball.position.y <= ball.size))
+			ball.collide('b');
+
+		// TODO: fazer o end-game com win ou lose
+
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawFPS(20, 20);
+		DrawText(textLifes.c_str(), screenW - 100, 20, 20, WHITE);
+		if(noBricks != 50 && !(ball.lifes <= 0)) {
+			for(const auto& b : bricks) {
+				DrawRectangleRec(b, BLUE);
+			}
+			DrawRectangleRec(player, WHITE);
+			DrawCircle(ball.position.x, ball.position.y, ball.size, RED);
+		}
+		else {
+			std::string stateWL{};
+			if(noBricks == 50) stateWL = "WIN";
+			else stateWL = "LOSE";
+
+			DrawText(stateWL.c_str(), screenW / 2 - 60, screenH / 2, 40, WHITE);
+
+			count == 200 ? exitWindow = true : count++;
+			std::cout << count << std::endl;
+		}
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
+
+void genScenario(std::vector<Rectangle>& brics) {
+	int ct{ 0 };
+	float H{ 50.0f }, W{ 10.0f };
+	for(int i{ 0 }; i < 50; i++) {
+		if(ct == 10) {
+			ct = 0;
+			H += 30.0f;
+			W = 10.0f;
+		}
+		brics[i].x = W;
+		brics[i].y = H;
+		brics[i].width = 50;
+		brics[i].height = 20;
+		W += 60.0f;
+		ct++;
+	}
+}
---
+++ GnsTwo/GnsTwo.h
@@ -0,0 +1,46 @@
+﻿// GnsTwo.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vector>
+#include <format>
+#include <string>
+#include <iostream>
+#include "raylib.h"
+
+struct Ball {
+	Vector2 position{};
+	float size{ 10 };
+	int lifes{ 5 },
+		xdirection{ 1 },
+		ydirection{ 1 };
+
+	void move(int screenW, int screenH) {
+		position.x += xdirection * 2;
+		position.y += ydirection * 2;
+	}
+
+	void collide(char who, int direction = 0) {
+		if(who == 'p') {
+			if(direction != 0) xdirection = direction;
+			else xdirection == 1 ? xdirection = -1 : xdirection = 1;
+			
+			ydirection == 1 ? ydirection = -1 : ydirection = 1;
+		}
+		else if(who == 'b') {
+			ydirection == 1 ? ydirection = -1 : ydirection = 1;
+		}
+		else if(who == 'w') {
+			xdirection == 1 ? xdirection = -1 : xdirection = 1;
+		}
+	}
+
+	void hit(int screenW, int screenH) {
+		lifes--;
+		position.x = screenW / 2;
+		position.y = screenH / 2;
+	}
+};
+
+void genScenario(std::vector<Rectangle>&);
